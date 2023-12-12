24a9644d76c3f9c8ceaf7928b312166922705953
autostopbydreamphase <kidisafraidtobealone@gmail.com>
2023-12-12 12:36:35 +0300

    12.12

--- js/main.js
+++ js/main.js
@@ -5,19 +5,16 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
-
 window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
-
 const src = ScrollReveal({
   distance: "60px",
   duration: 800,
   delay: 200,
   reset: true,
 });
-
 src.reveal(".text", { delay: 100, origin: "top" });
 src.reveal(".form-container form", { delay: 300, origin: "left" });
 src.reveal(".heading", { delay: 300, origin: "top" });
@@ -34,7 +31,6 @@ function showLogForm(state) {
   document.getElementById("logWindow").style.display = state;
   document.getElementById("grey-background2").style.display = state;
 }
-
 function closeRegForm(state) {
   document.getElementById("regWindow").style.display = state;
   document.getElementById("grey-background1").style.display = state;
@@ -43,8 +39,12 @@ function closeLogForm(state) {
   document.getElementById("logWindow").style.display = state;
   document.getElementById("grey-background2").style.display = state;
 }
-
-function addToLocalStorage() {
+function showOfferForm(state) {
+  document.getElementById("offerWindow").style.display = state;
+  document.getElementById("grey-background3").style.display = state;
+}
+function addToLocalStorage(event) {
+  event.preventDefault();
   var passwordConfirmationValue = document.getElementById(
     "passwordConfirmation"
   ).value;
@@ -56,15 +56,10 @@ function addToLocalStorage() {
   var passwordValue = document.getElementById("password").value;
   var emailValue = document.getElementById("e-mail").value;
   var phoneNumberValue = document.getElementById("phoneNumber").value;
-
-  // Регулярное выражение для проверки пароля
   var passwordRegex =
-    /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g;
-  // Регулярное выражение для проверки почты (простое общее выражение)
+    /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*_]{8,}/g;
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  // Проверка пароля на валидацию
   var isPasswordValid = passwordRegex.test(passwordValue);
-  // Проверка почты на валидацию
   var isEmailValid = emailRegex.test(emailValue);
   if (
     passwordConfirmationValue === passwordValue &&
@@ -79,13 +74,86 @@ function addToLocalStorage() {
       phoneNumber: phoneNumberValue,
     };
     localStorage.setItem("user", JSON.stringify(obj));
-    window.location.href = "../main.html#open-form";
+    window.location.href = "../main.html";
     console.log("Объект добавлен в локальное хранилище.");
+    passwordConfirmationField.value = "";
+    emailConfirmationField.value = "";
   } else {
     console.log(
       "Пароли или электронные почты не совпадают или не соответствуют требуемому формату."
     );
+    if (passwordConfirmationValue !== passwordValue) {
+      passwordConfirmationField.value = "";
+    }
+    if (emailConfirmationValue !== emailValue || !isEmailValid) {
+      emailConfirmationField.value = "";
+    }
   }
 }
+function checkFromLocalStorage(event) {
+  event.preventDefault();
+  var emailValue = document.getElementById("emailLogin").value;
+  var passwordValue = document.getElementById("passwordLogin").value;
+  var storedUser = localStorage.getItem("user");
+  var storedUserData = storedUser ? JSON.parse(storedUser) : null;
+  if (storedUserData) {
+    if (
+      emailValue === storedUserData.email &&
+      passwordValue === storedUserData.password
+    ) {
+      window.location.href = "../main.html";
+    } else {
+      console.log("Неверный email или пароль");
+    }
+  } else {
+    console.log("Пользователь не зарегистрирован");
+  }
+}
+document
+  .getElementById("userEmail")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    toggleDropdown();
+  });
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("show");
 
-function checkFromLocalStorage() {}
+  var mainButton = document.getElementById("userEmail");
+  mainButton.classList.toggle("open");
+}
+window.addEventListener("click", function (event) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var mainButton = document.getElementById("userEmail");
+  if (
+    !event.target.matches(".dropdown-content") &&
+    !event.target.matches("#userEmail")
+  ) {
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        mainButton.classList.remove("open");
+      }
+    }
+  }
+});
+function adjustDropdownWidth() {
+  var dropdown = document.getElementById("myDropdown");
+  var buttonWidth = document.getElementById("submitButton").offsetWidth;
+  dropdown.style.width = buttonWidth + "px";
+}
+
+window.addEventListener("resize", adjustDropdownWidth);
+adjustDropdownWidth();
+
+var myLink = document.getElementById("userEmail");
+const storageUserString = localStorage.getItem("user");
+const storedUser = JSON.parse(storageUserString);
+if (storedUser && storedUser.email) {
+  myLink.textContent = storedUser.email;
+}
+
+function signOut() {
+  window.location.href = "../index.html";
+}
