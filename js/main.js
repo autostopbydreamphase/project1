b25a76dae6fb10f371c2c06a07a4ec052b5ceaaa
autostopbydreamphase <kidisafraidtobealone@gmail.com>
2023-12-27 13:00:26 +0300

    27.12

--- js/main.js
+++ js/main.js
@@ -15,6 +15,16 @@ const src = ScrollReveal({
   delay: 200,
   reset: true,
 });
+const carsPrices = {
+  TeslaMartinKatler: 458,
+  TeslaX: 538,
+  Porsche911: 1198,
+  TeslaModelSPlaid: 830,
+  TeslaCharlieDeets: 358,
+  MercedesAMGGT63: 2640,
+  BMWM4: 870,
+  BMWM8: 1530,
+};
 src.reveal(".text", { delay: 100, origin: "top" });
 src.reveal(".form-container form", { delay: 300, origin: "left" });
 src.reveal(".heading", { delay: 300, origin: "top" });
@@ -28,21 +38,43 @@ function showRegForm(state) {
   document.getElementById("grey-background1").style.display = state;
 }
 function showLogForm(state) {
+  event.preventDefault();
   document.getElementById("logWindow").style.display = state;
   document.getElementById("grey-background2").style.display = state;
 }
-function closeRegForm(state) {
-  document.getElementById("regWindow").style.display = state;
-  document.getElementById("grey-background1").style.display = state;
-}
-function closeLogForm(state) {
-  document.getElementById("logWindow").style.display = state;
-  document.getElementById("grey-background2").style.display = state;
-}
-function showOfferForm(state) {
+
+function showOfferForm(carModel, state) {
   document.getElementById("offerWindow").style.display = state;
   document.getElementById("grey-background3").style.display = state;
+  console.log(carModel);
+  var selectedCar = document.querySelector(".select-selected");
+  selectedCar.textContent = carModel;
+  var carDropdown = document.getElementById("carDropdown");
+  carDropdown.dataset.value = carModel;
 }
+
+function showOfferForm(state) {
+  var offerWindow = document.getElementById("offerWindow");
+  var greyBackground = document.getElementById("grey-background3");
+
+  if (state === "none") {
+    offerWindow.classList.remove("visible");
+
+    setTimeout(function () {
+      offerWindow.style.display = "none";
+      greyBackground.style.display = "none";
+    }, 400);
+  } else {
+    offerWindow.style.display = "block";
+    greyBackground.style.display = "block";
+
+    setTimeout(function () {
+      offerWindow.classList.add("visible");
+    }, 50);
+  }
+}
+
+
 function addToLocalStorage(event) {
   event.preventDefault();
   var passwordConfirmationValue = document.getElementById(
@@ -157,3 +189,128 @@ if (storedUser && storedUser.email) {
 function signOut() {
   window.location.href = "../index.html";
 }
+
+function goBack(event) {
+  event.preventDefault();
+  window.history.back();
+}
+
+function carDropdown() {
+  var selectItems = document.querySelectorAll(".select-items div");
+  var selectedCar = document.querySelector(".select-selected");
+  var selectDropdown = document.querySelector(".select-items");
+  var infoAboutOffer = document.querySelector(".infoAboutOffer");
+
+  selectItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      selectedCar.textContent = this.textContent;
+      selectDropdown.classList.remove("show");
+      updateOfferInfo(
+        selectedCar.textContent,
+        document.querySelector(".select-selected2").textContent
+      );
+    });
+  });
+
+  selectedCar.addEventListener("click", function () {
+    selectDropdown.classList.toggle("show");
+  });
+  document.addEventListener("click", function (e) {
+    if (!e.target.matches(".select-selected")) {
+      var dropdowns = document.getElementsByClassName("select-items");
+      for (var d = 0; d < dropdowns.length; d++) {
+        var openDropdown = dropdowns[d];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  });
+
+  function updateOfferInfo(selectedCar, selectedTariff) {
+    var offerText = "";
+    if (selectedCar === "Tesla X" && selectedTariff === "МИНУТЫ") {
+      offerText = "Сумма заказа для Tesla X с тарифом МИНУТЫ: ... (ваш расчет)";
+    } else if (
+      selectedCar === "Porsche 911 Turbo S" &&
+      selectedTariff === "ЧАСЫ И КМ"
+    ) {
+      offerText =
+        "Сумма заказа для Porsche 911 Turbo S с тарифом ЧАСЫ И КМ: ... (ваш расчет)";
+    }
+
+    // другие условия для других автомобилей и тарифов
+
+    infoAboutOffer.textContent = offerText;
+  }
+}
+
+function tariffDropdown() {
+  var selectItems = document.querySelectorAll(".select-items2 div");
+  var selectedTariff = document.querySelector(".select-selected2");
+  var selectDropdown = document.querySelector(".select-items2");
+  var infoAboutOffer = document.querySelector(".infoAboutOffer");
+
+  selectItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      selectedTariff.textContent = this.textContent;
+      selectDropdown.classList.remove("show");
+
+      updateOfferInfo(
+        document.querySelector(".select-selected").textContent,
+        selectedTariff.textContent
+      );
+    });
+  });
+
+  selectedTariff.addEventListener("click", function () {
+    selectDropdown.classList.toggle("show");
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!e.target.matches(".select-selected2")) {
+      var dropdowns = document.getElementsByClassName("select-items2");
+      for (var d = 0; d < dropdowns.length; d++) {
+        var openDropdown = dropdowns[d];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  });
+
+  function updateOfferInfo(selectedCar, selectedTariff) {
+    var offerText = "";
+    if (selectedCar === "Tesla X" && selectedTariff === "МИНУТЫ") {
+      offerText = "Сумма заказа для Tesla X с тарифом МИНУТЫ: ... (ваш расчет)";
+    } else if (
+      selectedCar === "Porsche 911 Turbo S" &&
+      selectedTariff === "ЧАСЫ И КМ"
+    ) {
+      offerText =
+        "Сумма заказа для Porsche 911 Turbo S с тарифом ЧАСЫ И КМ: ... (ваш расчет)";
+    }
+
+    // другие условия для других автомобилей и тарифов
+
+    infoAboutOffer.textContent = offerText;
+  }
+}
+
+function validateDates() {
+  var startDate = document.getElementById("dateStart").value;
+  var endDate = document.getElementById("dateEnd").value;
+  if (new Date(startDate) >= new Date(endDate)) {
+    alert("Дата начала аренды должна быть раньше даты окончания аренды.");
+    return false;
+  }
+  return true;
+}
+document.querySelector("form").addEventListener("submit", function (event) {
+  if (!validateDates()) {
+    event.preventDefault();
+  }
+});
+
+carDropdown();
+tariffDropdown();
